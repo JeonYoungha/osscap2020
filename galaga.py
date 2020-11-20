47d60b9c6b70971d104916113b412daa3aa0edec
kimin-u <70623862+kimin-u@users.noreply.github.com>
2020-11-20 21:27:31 +0900

    Create galaga.py
    matrix 이용해서 대략적인 형태 만들어 봤고, 스페이스바 눌렀을때 time.sleep이용해서 좌표 이동하는 코드랑 
    부숴야 하는 장애물들 만들면 됨.
    부숴야 하는 장애물들도 초마다 좌우로 움직이게 해야 게임이 될듯

---
+++ galaga.py
@@ -0,0 +1,76 @@
+from matrix import *
+
+def draw_matrix(m):
+    array = m.get_array()
+    for y in range(m.get_dy()):
+        for x in range(m.get_dx()):
+            if array[y][x] == 0:
+                print("□", end='')
+            elif array[y][x] == 1:
+                print("■", end='')
+            else:
+                print("XX", end='')
+        print()
+
+flight =[[0,1,1],[1,1,0],[0,1,1]]
+
+iScreenDy=16
+iScreenDx=32
+top=7
+left = 27
+
+ArrayScreen=[
+               [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],       #0
+               [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],   
+               [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],       #2
+               [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+               [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],       #4
+               [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+               [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],       #6
+               [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+               [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],       #8
+               [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+               [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],       #10
+               [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+               [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],       #12
+               [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+               [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],       #14
+               [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]      
+            ]
+iScreen = Matrix(ArrayScreen)
+oScreen = Matrix(iScreen)
+flightBlk=Matrix(flight)
+tempBlk = iScreen.clip(top, left, top+flightBlk.get_dy(), left+flightBlk.get_dx())
+tempBlk = tempBlk + flightBlk
+oScreen.paste(tempBlk, top, left)
+draw_matrix(oScreen);print()
+
+
+#공을 발사하는 함수 정의해야 할듯
+
+while True:
+    key = input('Enter a key from [ q : quit, a : move left, d : move right, \' \' : shoot] : ')
+    if key == 'q':   # exit; 
+        print('Game terminated...')
+        break
+    elif key == 'a': # move left
+        top+=1
+    elif key == 'd': # move right
+        top-=1
+    elif key == ' ': # shoot 
+        continue    #not implemented; flight의 (1,0)좌표에서부터 왼쪽으로 쭉 이동하게끔 time.sleep 사용하면 될 듯
+        # 공 발사 함수 사전에 정의하고 쓰면 될 듯 
+    tempBlk = iScreen.clip(top, left, top+flightBlk.get_dy(), left+flightBlk.get_dx())
+    tempBlk = tempBlk + flightBlk
+    if tempBlk.anyGreaterThan(1):
+        if key == 'a': # undo: move right
+            top-= 1
+        elif key == 'd': # undo: move left
+            top+=1
+        
+        tempBlk = iScreen.clip(top, left, top+flightBlk.get_dy(), left+flightBlk.get_dx())
+        tempBlk = tempBlk + flightBlk
+    oScreen = Matrix(iScreen)
+    oScreen.paste(tempBlk, top, left)
+    draw_matrix(oScreen); print()
+
